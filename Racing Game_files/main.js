09b3c82565340892c3df90987f88778531a3b463
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-01 17:53:15 -0800

    FINALLY got the icons to move. Lane one okay, lane two needs work

---
+++ Racing Game_files/main.js
@@ -0,0 +1,8 @@
+// sanity check:
+console.log("main.js is linked");
+
+$(".lane-1").on("click", function movePlayer1(e){
+  // var laneOnePosition = $(this).box;
+  $(".box").text("x");
+  // var i = i + 1;
+})
--- main.js
+++ main.js
@@ -1,15 +1,57 @@
 // sanity check:
 console.log("main.js is linked");
 
-var $laneOneBoxArr = $(".laneOneBox");
+// var i = 1;
+// $(".laneOneRow").on("click", function laneOne(e){
+//   // console.log("i",i)
+//   $(".box"+i).html("here");
+//   i++;
+// });
 
+var i = 2;
 $(".laneOneRow").on("click", function laneOne(e){
-// move right
-// clear previous
-  // var i = 1;
-  $(".playerOne").appendTo("laneOneBox").eq(3);
-  // var i = i + 1
+  // console.log("i",i)
+  $(".box"+i).append("<img src='imgs/running-rabbit.png'> ");
+  i++;
 });
+
+var i = 2;
+$(".laneOneRow").on("keypress", function laneOne(e){
+  // console.log("i",i)
+  $(".box"+i).append("<img src='imgs/tortoise.png'> ");
+  i++;
+});
+
+
+
+
+// $(".laneOneRow").on("click", function laneOne(e){
+// // move right
+// // clear previous
+//   var i = 1;
+//   var boxNum = (".box" + i);
+//   $(boxNum).append($(".playerOne"));
+//   var i = i + 1;
+// });
+
+// $(".laneOneRow").on("click", function laneOne(e){
+//   var i = 2;
+//   var boxNum = ("box" + i);
+//   $(".playerOne").appendTo(boxNum);
+//   // var i = i + 1
+// });
+
+// $(".laneOneRow").on("click", function laneOne(e){
+//   var i = 3;
+//   var boxNum = ("box" + i);
+//   $(".playerOne").appendTo("boxNum");
+//   // var i = i + 1
+// });
+
+
+
+
+
 
  // for (var i = 0; i < $laneOneBoxArr.length; i++){
  //    $(".laneOneBox").eq(i).text("x");
