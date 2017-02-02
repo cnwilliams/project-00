b9bdf02275ab5f45450db1e273b681769366341b
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-01 19:55:47 -0800

    both pawns appear and disappear; rabbit stays in last box, tortoise does not

--- main.js
+++ main.js
@@ -18,26 +18,37 @@ console.log("main.js is linked");
 //   i++;
 // });
 
-var i = 2;
-$(".laneOneRow").on("click", function laneOne(e){
-  // console.log("i",i)
-  $(".laneOneBox.box"+i).append("<img src='imgs/running-rabbit.png'> ");
-  i++;
-});
+
+
+// BROKEN CODE BELOW
+// var i = 2;
+// $(document).on("keypress", function loneOne(event){
+//     if(event.key === "l"){
+//     $(".laneOneBox.box"+ x ).append("<img src='imgs/trunning-rabbit.png'> ");
+//     i++;
+//   }
+// });
 
 
 // CORRECT CODE BELOW
-// var i = 2;
-// $(".laneOneRow").on("click", function laneOne(e){
-//   // console.log("i",i)
-//   $(".laneOneBox.box"+i).append("<img src='imgs/running-rabbit.png'> ");
-//   i++;
-// });
+var i = 2;
+$(document).on("click", function laneOne(e){
+
+  $(".laneOneBox.box" + i).append("<img src='imgs/running-rabbit.png'> ");
+
+  if (i < 13){
+    $(".laneOneBox.box" + (i - 1) + " img").remove();
+  }
+
+  i++;
+});
 
 var x = 2;
 $(document).on("keypress", function laneTwo(event){
     if(event.key === "a"){
     $(".laneTwoBox.box"+ x ).append("<img src='imgs/tortoise.png'> ");
+    $(".laneTwoBox.box" + (x - 1) + " img").remove();
+
     x++;
   }
 });
